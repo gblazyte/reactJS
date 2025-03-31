77ec500385d990282754cecd932f138a8bbf7737
Gabriele <gblazyte8@gmail.com>
2025-03-31 10:33:27 +0300

    added book components

---
+++ final_project/src/components/BookCard.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class BookCard extends Component {
+    render() {
+        const { book } = this.props;
+        return (
+            <div className="book-card">
+                <h3>{book.title}</h3>
+                <p>{book.author}</p>
+                <Link to={`/books/${book.id}`}>View Details</Link>
+            </div>
+        );
+    }
+}
+
+export default BookCard;
